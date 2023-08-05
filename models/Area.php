325310cc212297b74f36b522f9ffed1886947faf
kenser <koffy140221@gmail.com>
2023-08-04 23:41:09 -0600

    crear modelo de area

---
+++ models/Area.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Model;
+
+class Area extends ActiveRecord{
+    public static $tabla = 'areas';
+    public static $columnasDB = ['area_nombre','area_situacion'];
+    public static $idTabla = 'area_id';
+
+    public $area_id;
+    public $area_nombre;
+    public $area_situacion;
+
+    public function __construct($args =[])
+    {
+        $this->area_id = $args['area_id'] ?? null;
+        $this->area_nombre = $args['area_nombre'] ?? '';
+        $this->area_situacion = $args['area_situacion'] ?? '1';
+    }
+
+}
