9060bda84eac9e409955cdac9e29f05c5075cfd8
kenser <koffy140221@gmail.com>
2023-08-04 22:17:47 -0600

    mantenimiento del modelo de empleados

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,23 +1,23 @@
 CREATE TABLE empleados (
-    emp_id SERIAL NOT NULL,
-    emp_nombre VARCHAR(100) NOT NULL,
-    emp_dpi INTEGER NOT NULL,
-    emp_id_puesto INTEGER NOT NULL,
-    emp_edad INTEGER NOT NULL,
-    emp_sexo VARCHAR (100) NOT NULL,
-    emp_id_area INTEGER NOT NULL,
-    emp_situacion smallint not null default 1,
-    PRIMARY KEY(emp_id),
-    FOREIGN KEY (emp_id_puesto) REFERENCES puestos(pue_id),
-    FOREIGN KEY (emp_id_area) REFERENCES areas(area_id),
+    empleado_id SERIAL NOT NULL,
+    empleado_nombre VARCHAR(100) NOT NULL,
+    empleado_dpi INTEGER NOT NULL,
+    empleado_id_puesto INTEGER NOT NULL,
+    empleado_edad INTEGER NOT NULL,
+    empleado_sexo VARCHAR (100) NOT NULL,
+    empleado_id_area INTEGER NOT NULL,
+    empleado_situacion smallint not null default 1,
+    PRIMARY KEY(empleadoleado_id),
+    FOREIGN KEY (empleado_id_puesto) REFERENCES puestos(puesto_id),
+    FOREIGN KEY (empleado_id_area) REFERENCES areas(area_id),
 );
 
 CREATE TABLE puestos  ( 
-    pue_id SERIAL NOT NULL,
-    pue_descripcion VARCHAR(100) NOT NULL,
-    pue_sueldo DECIMAL(8,2),
-    pue_situacion smallint not null default 1,
-    PRIMARY KEY(pue_id)
+    puesto_id SERIAL NOT NULL,
+    puesto_descripcion VARCHAR(100) NOT NULL,
+    puesto_sueldo DECIMAL(8,2),
+    puesto_situacion smallint not null default 1,
+    PRIMARY KEY(puesto_id)
 );
 
 CREATE TABLE areas  ( 
