1f53a28d091be0273c3f035a53fe5e8e7b208269
kenser <koffy140221@gmail.com>
2023-08-04 21:35:10 -0600

    crear archivos, para el proyecto, base de datos

---
+++ base_de_datos.sql
@@ -0,0 +1,28 @@
+CREATE TABLE empleados (
+    emp_id SERIAL NOT NULL,
+    emp_nombre VARCHAR(100) NOT NULL,
+    emp_dpi INTEGER NOT NULL,
+    emp_id_puesto INTEGER NOT NULL,
+    emp_edad INTEGER NOT NULL,
+    emp_sexo VARCHAR (100) NOT NULL,
+    emp_id_area INTEGER NOT NULL,
+    emp_situacion smallint not null default 1,
+    PRIMARY KEY(emp_id),
+    FOREIGN KEY (emp_id_puesto) REFERENCES puestos(pue_id),
+    FOREIGN KEY (emp_id_area) REFERENCES areas(area_id),
+);
+
+CREATE TABLE puestos  ( 
+    pue_id SERIAL NOT NULL,
+    pue_descripcion VARCHAR(100) NOT NULL,
+    pue_sueldo DECIMAL(8,2),
+    pue_situacion smallint not null default 1,
+    PRIMARY KEY(pue_id)
+);
+
+CREATE TABLE areas  ( 
+    area_id SERIAL NOT NULL,
+    area_nombre VARCHAR(200) NOT NULL,
+    area_situacion smallint not null default 1,
+    PRIMARY KEY(area_id)
+);
