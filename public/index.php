e5bc0857ae08cd5bea5cf04dcc9162422c22050b
kenser <koffy140221@gmail.com>
2023-08-04 23:14:39 -0600

    api puestos

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\EmpleadoController;
+use Controllers\PuestoController;
 
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
@@ -15,6 +16,12 @@ $router->post('/API/empleados/guardar', [EmpleadoController::class,'guardarAPI']
 $router->get('/API/empleados/buscar', [EmpleadoController::class,'buscarAPI']);
 $router->post('/API/empleados/modificar', [EmpleadoController::class,'modificarAPI']);
 $router->post('/API/empleados/eliminar', [EmpleadoController::class,'eliminarAPI']);
+
+$router->get('/puestos', [PuestoController::class,'index']);
+$router->post('/API/puestos/guardar', [PuestoController::class,'guardarAPI']);
+$router->get('/API/puestos/buscar', [PuestoController::class,'buscarAPI']);
+$router->post('/API/puestos/modificar', [PuestoController::class,'modificarAPI']);
+$router->post('/API/puestos/eliminar', [PuestoController::class,'eliminarAPI']);
 
 
 
