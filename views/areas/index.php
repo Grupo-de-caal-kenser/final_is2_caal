92efc907ba63de2a753e5079b2efa509e2a1ec9e
kenser <koffy140221@gmail.com>
2023-08-04 23:53:40 -0600

    crear vistas de areas y modificar vista de puestos

--- views/areas/index.php
+++ views/areas/index.php
@@ -1,22 +1,16 @@
-<h1 class="text-center">Formulario de puestos</h1>
+<h1 class="text-center">Formulario de area</h1>
 <div class="row justify-content-center mb-5">
-    <form class="col-lg-8 border bg-light p-3" id="formularioPuesto">
-        <input type="hidden" name="puesto_id" id="puesto_id">
+    <form class="col-lg-8 border bg-light p-3" id="formularioArea">
+        <input type="hidden" name="area_id" id="area_id">
         <div class="row mb-3">
                 <div class="col">
-                    <label for="puesto_descripcio">Nombre del puesto</label>
-                    <input type="text" name="puesto_descripcion" id="puesto_descripcion" class="form-control">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col">
-                    <label for="puesto_sueldo">SUELDO</label>
-                    <input type="number" name="puesto_sueldo" id="puesto_sueldo" class="form-control">
+                    <label for="area_nombre">Nombre del area</label>
+                    <input type="text" name="area_nombre" id="area_nombre" class="form-control">
                 </div>
             </div>
         <div class="row mb-3">
             <div class="col">
-                <button type="submit" form="formularioPuesto" id="btnGuardar" data-saludo= "hola" data-saludo2="hola2" class="btn btn-primary w-100">Guardar</button>
+                <button type="submit" form="formularioArea" id="btnGuardar" data-saludo= "hola" data-saludo2="hola2" class="btn btn-primary w-100">Guardar</button>
             </div>
             <div class="col">
                 <button type="button" id="btnModificar" class="btn btn-warning w-100">Modificar</button>
@@ -32,13 +26,12 @@
 </div>
 <div class="row justify-content-center" id="divTabla">
     <div class="col-lg-8">
-        <h2>Listado de puestos</h2>
-        <table class="table table-bordered table-hover" id="tablaPuestos">
+        <h2>Listado de areas</h2>
+        <table class="table table-bordered table-hover" id="tablaAreas">
             <thead class="table-dark">
                 <tr>
                     <th>NO. </th>
                     <th>NOMBRE</th>
-                    <th>SUELDO</th>
                     <th>MODIFICAR</th>
                     <th>ELIMINAR</th>
                 </tr>
@@ -48,4 +41,4 @@
         </table>
     </div>
 </div>
-<script src="<?= asset('./build/js/puestos/index.js')  ?>"></script>
+<script src="<?= asset('./build/js/areas/index.js')  ?>"></script>
